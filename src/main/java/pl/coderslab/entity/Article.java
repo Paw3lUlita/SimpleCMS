728d4a7dda3220738f5e6769e97351aa2648b484
Paw3lUlita <pawel.ulita33@gmail.com>
2022-07-17 12:29:26 +0200

    Creating Entities

---
+++ src/main/java/pl/coderslab/entity/Article.java
@@ -0,0 +1,35 @@
+package pl.coderslab.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 200)
+    private String title;
+    @ManyToOne
+    private Author author;
+    @ManyToMany
+    private List<Category> categories;
+    private String content;
+
+
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+
+    @PrePersist
+    public void prePersist() {
+        created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updated = LocalDateTime.now();
+    }
+}
